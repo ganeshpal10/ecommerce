026bf1ce153533aeaa40564e3d4e34e0cd1b1031
ganeshpal10 <palganeshgp.05@gmail.com>
2021-09-16 11:33:23 +0530

    add accesories page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter,Route,Switch} from "react-router-dom";   
 import './app.css';
 import Home from "./components/home/home";
-// import Accesories from "./components/accesories/accesories";
+import Accesories from "./components/accesories/accesories";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route path="/accesories" component={Accesories}/> */}
+        <Route path="/accesories" component={Accesories}/>
         
         <Route path="/" component={Home}  />
       </Switch>
